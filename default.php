6a0c9465f8353eb1f20696cdb7e031e565e5f0d8
ThatOwlGuy <theOwlInbox@gmail.com>
2024-03-10 16:24:09 -0500

    added default.php

---
+++ default.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style>
+            center {
+                margin-top: 50vh;
+                font-family: "Futura";
+            }
+        </style>
+    </head>
+    <body>
+        <center>(OvO)</center>
+    </body>
+</html>
